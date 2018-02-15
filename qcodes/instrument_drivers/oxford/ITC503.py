f5beeb4bea727c61cbf57c2c4252cf4da63e9179
quantumCryoCoder <ste.poletto@gmail.com>
2018-02-15 18:19:36 +0100

    clean up oxford ITC503 driver
    The Oxford ITC503 ("intelligent temperature controller") is a fridge
    control box (impedance bridge plus heaters etc) .  It had a horrible
    driver.

--- qcodes/instrument_drivers/oxford/ITC503.py
+++ qcodes/instrument_drivers/oxford/ITC503.py
@@ -1,41 +1,40 @@
-from qcodes import Instrument, VisaInstrument, validators as vals
 from qcodes.instrument import visa as visa_mod
 
-class OxfordInstruments_ITC503(Instrument):
-    '''
-    Oxford Instruments Temprature Controller 503 driver.
-    
-    Implemented:
-                - Temperature reading from the 3 thermometer channels.
-    Not implemented:
-                - Heating controller.
-                - PID-controller for heater.
-    '''
-    def __init__(self, name, address, **kw):
-        super().__init__(name, **kw)
-        self._instr_instance = visa_mod.VisaInstrument('IT503_sub',
-        	                                           address=address,
-        	                                           terminator='\r')
-        self.add_parameter(name='ch1',
-                           label='ch1',
-                           unit='K',
-                           get_cmd=lambda : self._get_temp(1),
-                           get_parser=float,
-                           vals=vals.Numbers(0., 1e3))
-        self.add_parameter(name='ch2',
-                           label='ch2',
-                           unit='K',
-                           get_cmd=lambda : self._get_temp(2),
-                           get_parser=float,
-                           vals=vals.Numbers(0., 1e3))
-        self.add_parameter(name='ch3',
-                           label='ch3',
-                           unit='K',
-                           get_cmd=lambda : self._get_temp(3),
-                           get_parser=float,
-                           vals=vals.Numbers(0., 1e3))
+class OxfordInstruments_ITC503(visa_mod.VisaInstrument):
+  def __init__(self, name, address):
+    super().__init__(name, address, terminator='\r')
 
-    def _get_temp(self, channel):
-    	msg = self._instr_instance.ask('R%d'%channel)
-    	return float(msg[1:])
+    def parse_response(s):
+      "chop off the first letter, as instrument answers 'Rx.xxx'"
+      return float(s[1:])
 
+    self.add_parameter(name='sorb',
+                           label='Sorb temperature',
+                           unit='K',
+                           get_cmd="R1",
+                           get_parser=parse_response)
+    self.add_parameter(name='ROX',
+                       label='ROX temperature',
+                       unit='K',
+                       get_cmd="R2",
+                       get_parser=parse_response)
+    self.add_parameter(name='CERNOX',
+                       label='CERNOX temperature',
+                       unit='K',
+                       get_cmd="R3",
+                       get_parser=parse_response)
+
+  def get_idn(self):
+
+      #the ITC does not have an IDN command implemented, but it has a "V" (for version) command,
+      #so we fake it
+
+      s = self.ask("V")
+
+      import parse
+
+      idn = parse.parse("{model} Version {firmware} (c) {vendor} {}", s).named
+
+      idn["serial"] = None
+
+      return idn
